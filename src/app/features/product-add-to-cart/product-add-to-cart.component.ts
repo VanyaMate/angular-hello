f3c63542eed192eaad80122f4532ee52dbc7d83f
VanyaMate <buhu653@mail.ru>
2024-01-24 18:24:31 +0300

    up add-to-cart

--- src/app/features/product-add-to-cart/product-add-to-cart.component.ts
+++ src/app/features/product-add-to-cart/product-add-to-cart.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 
 @Component({
     selector   : 'app-product-add-to-cart',
     standalone : true,
-    imports    : [],
+    imports    : [
+        NgClass,
+    ],
     templateUrl: './product-add-to-cart.component.html',
     styleUrl   : './product-add-to-cart.component.scss',
 })
 export class ProductAddToCartComponent {
     @Input() id: string | undefined;
 
+    protected pending: boolean = false;
+
     protected addToCart (): void {
-        console.log(this.id);
+        this.pending = true;
+        setTimeout(() => {
+            this.pending = false;
+        }, 1000);
     }
 }
